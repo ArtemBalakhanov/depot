81883809552c7c6c28e351870e4fc19403bba9cf
Artem <artem.balakhanov@mail.ru>
2017-07-23 18:50:17 +0300

    devise work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
+before_filter :authenticate_user!, :except => [:show, :index]
 before_action :set_product, only: [:show, :edit, :update, :destroy]
+
 
 
   # GET /products
